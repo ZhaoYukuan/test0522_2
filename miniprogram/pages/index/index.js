da586d2840f1d72dcf8ca5022121e3e8844b6b49
赵禹宽 <zhao_yukuan@qq.com>
2019-05-24 18:50:28 +0800

    190524

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -5,17 +5,6 @@ const {regeneratorRuntime} = global
 Page({
     data: {},
     onLoad() {
-        ;(async () => {
-            try {
-                let res = await utils_zyk.yRemove("a", {
-                    _openid:"oaWKa5RjCV0Idi39LA8udnXzXmLY"
-                })
-                utils_zyk.l("index.js", 11).c(res)
-
-            } catch (e) {
-                utils_zyk.c(e)
-            }
-        })();
     },
     getUserInfoOnPage() {
         ;(async () => {
