8a41b696005e3cae1e790357c6a6bfed01361f1c
赵禹宽 <zhao_yukuan@163.com>
2019-06-02 09:58:45 +0800

    0602

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -25,25 +25,14 @@ Page({
             url: "/pages/next/next"
         })
     },
-    tpjc() {
-        wx.chooseImage({
-            count: 1, // 默认9
-            // sizeType: ['compressed'], // 可以指定是原图还是压缩图，默认二者都有
-            sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-            success: function (photo) {
-                let tempFilesSize = photo.tempFiles[0].size; //获取图片的大小，单位B
-                if (tempFilesSize <= 3000000 && tempFilesSize > 50000) { //图片小于或者等于3M时 可以执行获取图片
-                    const src = photo.tempFilePaths[0]
-                    wx.navigateTo({
-                        url: `/pages/tpjc/tpjc?src=${src}&type=classPath`
-                    })
-                } else { //图片大于3M，弹出一个提示框
-                    wx.showToast({
-                        title: '请上传大小在50K-3M的图片!', //标题
-                        icon: 'none' //图标none不使用图标，详情看官方文档
-                    })
-                }
-            }
+    cropper() {
+        wx.navigateTo({
+            url: "/pages/cropper/cropper"
         })
     },
+    qrcode() {
+        wx.navigateTo({
+            url: "/pages/qrcode/qrcode"
+        })
+    }
 })
---
+++ miniprogram/pages/qrcode/qrcode.js
@@ -0,0 +1,16 @@
+import drawQrcode from '../../libs/qrcode/weapp.qrcode.esm.js'
+
+Page({
+    data: {
+        windowWidth: wx.getSystemInfoSync().windowWidth,
+        text: "test"
+    },
+    onLoad(option) {
+        drawQrcode({
+            width: this.data.windowWidth * 500 / 750,
+            height: this.data.windowWidth * 500 / 750,
+            canvasId: 'myQrcode',
+            text: this.data.text,
+        })
+    },
+})
